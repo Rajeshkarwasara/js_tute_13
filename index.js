e3b90407ab6d1d5b36d31dbf53bb14f449048114
Rajesh karwasara <140477625+Rajeshkarwasara@users.noreply.github.com>
2023-09-03 10:31:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let str = "hello Rajesh"
+
+let test = /rajesh/;
+
+let rj = test.test(str);
+console.log(rj);
+
+
+let rejex = "asfh 78648658"
+let chek = /[a-z]\s[1-9]{8}/;
+totel = chek.test(rejex);
+console.log(totel)
+
+
